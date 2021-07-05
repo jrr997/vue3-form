c15e30f0258ad72eaed03b7460fb9ca4fc6da7b4
ZackChang <997571692@qq.com>
2021-07-05 20:34:34 +0800

    test: ArrayField

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,10 @@
 import SchemaFrom from './SchemaForm'
 import NumberField from './fields/NumberField'
 import StringField from './fields/StringField'
+import ArrayField from './fields/ArrayField'
+import ObjectField from './fields/ObjectField'
+import SelectionWidget from './widgets/Selection'
 
 export default SchemaFrom
 
-export { NumberField, StringField }
+export { NumberField, StringField, SelectionWidget, ArrayField, ObjectField }
